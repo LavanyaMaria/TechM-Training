0430be01171fc15fa543eb12d37d454881fdc784
LavanyaMaria <141541464+LavanyaMaria@users.noreply.github.com>
2025-02-11 00:01:35 +0530

    Create stream.java

---
+++ JAVA/10-2-2025/assignmant 3/stream.java
@@ -0,0 +1,17 @@
+package org.example.assignment;
+import java.io.*;
+public class stream {
+    public static void main(String [] args)
+    {
+        System.out.print("System.print.out!\n");
+        String name="Jack Reacher";
+        System.out.print("Enter book index: "+ name);
+
+        System.out.println("Team 9 Dubs!");
+        System.out.println("TvD...TO....L");
+        System.out.println("Watch the "+name+" now on Netflix!");
+
+        System.err.print("Error: NoCodeFound");
+        System.err.println("Error: NoGo");
+    }
+}
