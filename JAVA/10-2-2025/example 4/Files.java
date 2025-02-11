b22c2772ba0c6604265670f2e4ff9cff88669a2e
LavanyaMaria <141541464+LavanyaMaria@users.noreply.github.com>
2025-02-12 00:07:14 +0530

    Create Files.java

---
+++ JAVA/10-2-2025/example 4/Files.java
@@ -0,0 +1,8 @@
+package org.example.assignment.feb10;
+import java.io.File;
+import java.util.Scanner;
+public class Files {
+    public static void main(String[] args){
+        System.out.println("");
+    }
+}
