62c65cda111f542cf81be6e779fe120a156c01d5
LavanyaMaria <141541464+LavanyaMaria@users.noreply.github.com>
2025-02-11 23:02:00 +0530

    Create Main.java

---
+++ JAVA/10-2-2025/example 2/Main.java
@@ -0,0 +1,11 @@
+package org.example.assignment.feb10.example2;
+
+public class Main {
+    public static void main(String[] args){
+        Welcome.wel();
+        To.to();
+        Club c=new Club();
+        c.club();
+
+    }
+}
