a8bfb117b3ae1990224949d9ff70f1ac0665e1ee
LavanyaMaria <141541464+LavanyaMaria@users.noreply.github.com>
2025-02-14 14:39:12 +0530

    Create task10.js

---
+++ JAVASCRIPT/14-02-25/task10.js
@@ -0,0 +1,12 @@
+let math=prompt("Enter the maths score:")
+if(math == 100){
+    console.log("Phenomenal!")
+}else if(math>85){
+    console.log("Excellent!")
+}else if(math>=71 && math<=85){
+    console.log("Good Work!")
+}else if(math>=51 && math<=70){
+    console.log("Practice Well!")
+}else{
+    console.log("Hard work is needed")
+}
