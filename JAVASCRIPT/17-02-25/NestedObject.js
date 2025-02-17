48894151f7bb717ae5f96bd6523447a782e3dc36
LavanyaMaria <141541464+LavanyaMaria@users.noreply.github.com>
2025-02-17 10:33:59 +0530

    Update NestedObject.js

--- JAVASCRIPT/17-02-25/NestedObject.js
+++ JAVASCRIPT/17-02-25/NestedObject.js
@@ -30,3 +30,23 @@ const person = {
 let name = person.fullName();
 console.log("Full name: "+name)
 console.log("fullName function definition: "+person.fullName);
+
+person.name1 = function () {
+  return this.firstName + " " + this.lastName;
+};
+
+person.name2 = function () {
+  return (this.firstName + " " + this.lastName).toUpperCase();
+};
+console.log(person.name1())
+console.log(person.name2())
+console.log("name1 function definition: "+person.name1);
+console.log("name2 function definition: "+person.name2);
+
+/*document.getElementById("demo").innerHTML = person;
+
+document.getElementById("demo").innerHTML =
+person.name + "," + person.age + "," + person.city;*/
+
+//Removing function from the object
+delete person.name1;
