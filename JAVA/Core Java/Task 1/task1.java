4734cede03b92e3d51730983745feed214c5aa93
LavanyaMaria <141541464+LavanyaMaria@users.noreply.github.com>
2025-02-22 11:59:39 +0530

    Update task1.java

--- JAVA/Core Java/Task 1/task1.java
+++ JAVA/Core Java/Task 1/task1.java
@@ -1,1 +1,30 @@
+package org.example.assignment.corejava.Task1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. of elements:");
+        int n=sc.nextInt();
+
+        ArrayList<Integer> a=new ArrayList<>();
+        System.out.println("Elements are: ");
+        for(int i=0;i<n;i++)
+        {
+            a.add(sc.nextInt());
+        }
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int i=0;i<a.size();i++){
+            if(i%2==0){
+                res.add(a.get(i));
+            }
+        }
+
+        System.out.println("Original array :"+a);
+        System.out.println("Alternative array : "+res);
+    }
+}
 
