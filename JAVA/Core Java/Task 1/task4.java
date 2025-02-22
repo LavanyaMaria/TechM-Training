26b156d45ecd8b15ed1d563a25fff310be89519e
LavanyaMaria <141541464+LavanyaMaria@users.noreply.github.com>
2025-02-22 16:06:03 +0530

    Create task4.java

---
+++ JAVA/Core Java/Task 1/task4.java
@@ -0,0 +1,14 @@
+package org.example.assignment.corejava.Task1;
+
+import java.util.Arrays;
+
+public class copyRange {
+    public static void main(String[] args){
+        int[] ar={1,2,3,4,5,6,7,6,5,4,3,2,1};
+
+        int[] copy= Arrays.copyOfRange(ar,2,8) ;
+
+        System.out.println("Original array: "+Arrays.toString(ar));
+        System.out.println("Copied array: "+Arrays.toString(copy));
+    }
+}
