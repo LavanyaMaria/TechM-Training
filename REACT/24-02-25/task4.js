6fabecd8ff84e7fa7d0a2fe02f11cb96d77c6742
LavanyaMaria <141541464+LavanyaMaria@users.noreply.github.com>
2025-02-24 14:09:14 +0530

    Update task4.js

--- REACT/24-02-25/task4.js
+++ REACT/24-02-25/task4.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Animal(props) {
-  return <h2>{props.species} Animal</h2>;
+function Animal() {
+  return <h2>Panda!</h2>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Animal  species="Panda"/>);
+root.render(<Animal />);
