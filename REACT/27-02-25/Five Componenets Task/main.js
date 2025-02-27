7b92683a4408df416d4b3eb86a7a70eccccb3833
LavanyaMaria <141541464+LavanyaMaria@users.noreply.github.com>
2025-02-27 13:30:47 +0530

    Update main.js

--- REACT/27-02-25/Five Componenets Task/main.js
+++ REACT/27-02-25/Five Componenets Task/main.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 //import App from "./App.jsx";
 //import Lavender from "./assets/FlowerShop/Lavender.jsx";
-import ABComponent2 from "./assets/FlowerShop/s.jsx";
 import Lily from "./assets/FlowerShop/Lily.jsx";
 
 /*createRoot(document.getElementById("root")).render(
@@ -13,9 +12,6 @@ import Lily from "./assets/FlowerShop/Lily.jsx";
 );*/
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div>
       <Lily />
-      <ABComponent2 />
-    </div>
   </StrictMode>
 );
